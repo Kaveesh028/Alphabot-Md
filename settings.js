57ebe4c795fa344f123ca470a8402c9719c334ec
Kaveesh028 <130683460+Kaveesh028@users.noreply.github.com>
2023-07-06 08:39:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = true // auto read message
+global.autoread = false // auto read message
 global.autobio = false //auto ganti bio
 global.anticall = true //anti call 
 global.userRegister = false
@@ -32,17 +32,17 @@ global.language = english //change indonesia to english if you don't understand 
 // Other
 // Other
 global.botname = "Alphabot-Md" //namabot kalian
-global.ownername = "sandaru" //nama kalian
-global.myweb = "https://s.id/BOTGITHUB" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@sandaru12" //bebas asal jan hapus
-global.myweb2 = "https://s.id/BOTGITHUB" //bebas
-global.email = "mdsandaru2@gmail.com" //bebas
+global.ownername = "KA VEE SH" //nama kalian
+global.myweb = "http://wa.me/94740075228?text=❝𝘏𝘌𝘠_𝘬𝘢𝘷𝘦𝘦_🩵💍🌈❞" //bebas asal jan hapus
+global.youtube = "https://youtube.com" //bebas asal jan hapus
+global.myweb2 = "http://wa.me/94740075228?text=❝𝘏𝘌𝘠_𝘬𝘢𝘷𝘦𝘦_🩵💍🌈❞" //bebas
+global.email = "crazyromeo028@gmail.com" //bebas
 global.region = "Buddhist" //bebas
 global.timezone = 'Asia/Colombo' //  timezone wib
-global.premium = ['94764497078'] //premium user
-global.owner = ["764497078", "94764497078"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "764497078" // nomor wa kalian
-global.ownernomerr = "+94764497078" //nmr wa kalian
+global.premium = ['94740075693'] //premium user
+global.owner = ["94740075693", "94740075693"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "94740075693" // nomor wa kalian
+global.ownernomerr = "+94740075693" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
